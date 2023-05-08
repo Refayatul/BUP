d570fa2329671daab04a46c7eae868672233112d
Md. Refayatul Islam <refayatul@outlook.com>
2023-05-09 03:18:50 +0600

    Update and rename G - Smart Member.c to 7 G - Smart Member.c

---
+++ CSE/Semister's/Semister 2/SPL/Asassignments/7 G - Smart Member.c
@@ -0,0 +1,22 @@
+//Refayatul_BUP_CSE_22
+https://codeforces.com/profile/refayatul
+https://www.codechef.com/users/refayatul
+
+#include <stdio.h>
+int main()
+{
+    int X,Y,N;
+    scanf("%d %d %d",&X,&Y,&N);
+
+    double c1,c2,c3;
+
+    c1=N*X;
+    c2=(N/3)*Y+(N%3)*X;
+
+    if(c1>=c2)
+        printf("%.0lf",c2);
+    else
+        printf("%.0lf",c1);
+
+    return 0;
+}

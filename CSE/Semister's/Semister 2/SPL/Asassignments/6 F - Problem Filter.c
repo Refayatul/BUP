c907eee962c9715400942796560e4943dc4aa020
Md. Refayatul Islam <refayatul@outlook.com>
2023-05-09 03:18:16 +0600

    Update and rename F - Problem Filter.c to 6 F - Problem Filter.c

---
+++ CSE/Semister's/Semister 2/SPL/Asassignments/6 F - Problem Filter.c
@@ -0,0 +1,29 @@
+//Refayatul_BUP_CSE_22
+https://codeforces.com/profile/refayatul
+https://www.codechef.com/users/refayatul
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n, i;
+    cin >> n;
+
+    while(n>0) {
+        cin >> i;
+        if (i == 1) {
+            cout << "HARD" << endl;
+            return 0; 
+        }
+        n--;
+    }    
+    cout << "EASY" << endl; 
+
+    return 0;
+}

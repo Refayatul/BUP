e292f20028c45030f05b4b01ecc79266d363eba5
Md. Refayatul Islam <refayatul@outlook.com>
2023-05-09 03:07:11 +0600

    Update and rename C - Ghost Hunter.c to 3_C - Ghost Hunter.cpp

---
+++ CSE/Semister's/Semister 2/SPL/Asassignments/3_C - Ghost Hunter.cpp
@@ -0,0 +1,16 @@
+//Refayatul_BUP_CSE_22
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--)
+	{
+	    int x;
+	    cin >> x;
+	    cout << (x >= 67 && x <= 45000 ? "YES" : "NO") << "\n";
+	}
+	return 0;
+}

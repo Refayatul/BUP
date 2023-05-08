62c08cd5765396d0100d325b35cb210fbd76a289
Md. Refayatul Islam <refayatul@outlook.com>
2023-05-09 03:24:49 +0600

    Update and rename H - Color Overlapping.c to 8 H - Color Overlapping.cpp

---
+++ CSE/Semister's/Semister 2/SPL/Asassignments/8 H - Color Overlapping.cpp
@@ -0,0 +1,20 @@
+/*Refayatul_BUP_CSE_22
+https://codeforces.com/profile/refayatul
+https://www.codechef.com/users/refayatul
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int l1, r1, l2, r2;
+	cin >> l1 >> r1 >> l2 >> r2;
+	if (l1 > l2) {
+		swap(l1, l2);
+		swap(r1, r2);
+	}
+	if (r1 <= l2)cout << 0 << endl;
+	else if (r1 <= r2)cout << r1 - l2 << endl;
+	else cout << r2 - l2 << endl;
+        // cout << max(0, min(r1, r2) - max(l1, l2)) << endl; (this is sufficient in fact)
+	return 0;
+}

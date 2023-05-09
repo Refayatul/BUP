729e7f224eeeea442ca6bb09d4dd633e8c499cb9
Md. Refayatul Islam <refayatul@outlook.com>
2023-05-09 10:03:52 +0600

    Update and rename O - Translation.c to 15 O - Translation.cpp

---
+++ CSE/Semister's/Semister 2/SPL/Asassignments/15 O - Translation.cpp
@@ -0,0 +1,27 @@
+/*Refayatul_BUP_CSE_22
+https://codeforces.com/profile/refayatul
+https://www.codechef.com/users/refayatul
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    reverse(t.begin(), t.end());
+    if (s == t)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}

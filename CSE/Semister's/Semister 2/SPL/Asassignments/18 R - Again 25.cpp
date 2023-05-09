e869cedef242c369b9b80541fec949dccafc1ef3
Md. Refayatul Islam <refayatul@outlook.com>
2023-05-09 10:07:31 +0600

    Update and rename R - Again 25.c to 18 R - Again 25.cpp

---
+++ CSE/Semister's/Semister 2/SPL/Asassignments/18 R - Again 25.cpp
@@ -0,0 +1,14 @@
+/*Refayatul_BUP_CSE_22
+https://codeforces.com/profile/refayatul
+https://www.codechef.com/users/refayatul
+*/
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    cout << 25 << endl;
+    return 0;
+}

3ec517b3f1a63e4a87f812b9285387442e67e8e0
Md. Refayatul Islam <refayatul@outlook.com>
2023-05-11 17:44:05 +0600

    Update 7 G - Smart Member.c

--- CSE/Semister's/Semister 2/SPL/Asassignments/7 G - Smart Member.c
+++ CSE/Semister's/Semister 2/SPL/Asassignments/7 G - Smart Member.c
@@ -2,21 +2,25 @@
 https://codeforces.com/profile/refayatul
 https://www.codechef.com/users/refayatul
 */
+
+
+
+
 #include <stdio.h>
+
 int main()
 {
-    int X,Y,N;
-    scanf("%d %d %d",&X,&Y,&N);
+    int X, Y, N;
+    scanf("%d %d %d", &X, &Y, &N);
 
-    double c1,c2,c3;
+    int c1, c2;
+    c1 = N * X;
+    c2 = (N / 3) * Y + (N % 3) * X;
 
-    c1=N*X;
-    c2=(N/3)*Y+(N%3)*X;
-
-    if(c1>=c2)
-        printf("%.0lf",c2);
+    if (c1 >= c2)
+        printf("%d", c2);
     else
-        printf("%.0lf",c1);
+        printf("%d", c1);
 
     return 0;
 }

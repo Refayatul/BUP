2f7ccda308b90591db86202f3b62f110ac0680fc
Md. Refayatul Islam <refayatul@outlook.com>
2023-05-11 18:26:35 +0600

    Update Z - ATM.c

--- CSE/Semister's/Semister 2/SPL/Asassignments/Z - ATM.c
+++ CSE/Semister's/Semister 2/SPL/Asassignments/Z - ATM.c
@@ -1,1 +1,24 @@
+/*Refayatul_BUP_CSE_22
+https://codeforces.com/profile/refayatul
+https://www.codechef.com/users/refayatul
+*/
 
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+    scanf("%d %f", &a, &b);
+    if ((a%5==0) && (b-a-0.5>=0))
+    {
+        printf("%.2f\n", b-a-0.5);
+    }
+    else
+    {
+        printf("%.2f\n", b);
+    }
+    return 0;
+}

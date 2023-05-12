bfa366ec43368550f480a28f36ad9e7903fef185
Md. Refayatul Islam <refayatul@outlook.com>
2023-05-12 11:52:48 +0600

    Update 39_AM - Pythagorean Theorem II.c

--- CSE/Semister's/Semister 2/SPL/Asassignments/39_AM - Pythagorean Theorem II.c
+++ CSE/Semister's/Semister 2/SPL/Asassignments/39_AM - Pythagorean Theorem II.c
@@ -1,1 +1,26 @@
+/*Refayatul_BUP_CSE_22
+https://codeforces.com/profile/refayatul
+https://www.codechef.com/users/refayatul
+*/
 
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n, cnt = 0;
+    scanf("%d", &n);
+
+    for (int a = 1; a <= n; a++)
+    {
+        for (int b = a + 1; b <= n; b++)
+        {
+            double c = sqrt(a * a + b * b);
+            if (floor(c) == c && c <= n)
+                cnt++;
+        }
+    }
+
+    printf("%d\n", cnt);
+    return 0;
+}
